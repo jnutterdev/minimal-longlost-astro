bc0abdda6a16f7ef3844a260847a36f9bf700e0f
jnutterdev <hello@jnutterdev.com>
2025-08-06 18:32:32 -0400

    Updating layout

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const SITE = {
   website: "https://longlostforgotten.com",
   author: "John Nutter",
   profile: "https://jnutterdev.com",
-  desc: "A minimal, blog on everything",
+  desc: "longlostforgotten: the terrible inconvenience of. A personal blog for sharing music, books, and pictures.",
   title: "longlostforgotten",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
@@ -10,14 +10,14 @@ export const SITE = {
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
-  showBackButton: true, // show back button in post detail
+  showBackButton: true,
   editPost: {
     enabled: true,
     text: "Edit page",
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
   dynamicOgImage: true,
-  dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "America/New_York", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  dir: "ltr",
+  lang: "en",
+  timezone: "America/New_York",
 } as const;
